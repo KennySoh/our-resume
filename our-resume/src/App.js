f333b38b56db2f926096e7864604d0d9b21ba596
kenny <kenny@otsaw.com.sg>
2022-04-21 21:43:42 +0800

    Header Component Added

--- our-resume/src/App.js
+++ our-resume/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { Header } from './common/components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       {/* Header */}
+      <Header></Header>
+      
       {/* Resume */}
       {/* BlogSpot */}
     </div>
